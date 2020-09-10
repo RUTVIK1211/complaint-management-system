4f8501ec716b3c5e84156c736919707726c51666
Rutvik <37297719+RUTVIK1211@users.noreply.github.com>
2020-09-10 11:58:24 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>this is index of user</title>
+</head>
+<body>
+    this is an siple php file 
+</body>
+</html>
