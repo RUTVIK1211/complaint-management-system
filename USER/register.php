3b6e00b943505e39c9bca29aa2cb0dd6103a88cc
Rutvik <bhimani.rutvik1211@gmail.com>
2020-09-13 12:23:23 +0530

    intial commit:

--- USER/register.php
+++ USER/register.php
@@ -1,10 +1,9 @@
 <?php require_once 'header.php'; ?>
 <div class="tm-container">
-
             <div class="tm-row">
                 <div class="tm-col-left"></div>
-                <main class="tm-col-right tm-contact-main">
-                	<!-- Content -->
+                <main clasdss="tm-col-right tm-contact-main">
+                	<!-- Constent -->
                     <section class="tm-content tm-contact">
                         <h3 class="mb-4 tm-content-title">Registeration</h3>
                         <form id="contact-form" action="" method="POST">
