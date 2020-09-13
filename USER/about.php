e965843a9a67017763f374669f492cdb8b42e77c
Rutvik <bhimani.rutvik1211@gmail.com>
2020-09-13 23:14:12 +0530

    heading with branch

--- USER/about.php
+++ USER/about.php
@@ -1,5 +1,5 @@
 <?php require_once 'header.php'; ?>
-   
+
     <div class="tm-row">
         <div class="tm-col-left"></div>
             <main class="tm-col-right">
@@ -10,26 +10,26 @@
                             <i class="fas fa-shapes fa-3x p-3 mr-4"></i>
                             <div class="media-body">
                                 <p>Provide a platform for student to share their problems.</p>
-                            </div> 
+                            </div>
                         </div>
                         <div class="media my-3">
                             <i class="fas fa-draw-polygon fa-3x p-3 mr-4"></i>
                             <div class="media-body">
                                 <p>Keep the identity of the sender hidden</p>
-                            </div> 
+                            </div>
                         </div>
                         <div class="media my-3">
                             <i class="fab fa-creative-commons-share fa-3x p-3 mr-4"></i>
                             <div class="media-body">
                                 <p>Allow user to check that whether their complain has been reviewed or not.</p>
-                            </div> 
+                            </div>
                         </div>
                         <div class="media my-3">
                             <i class="fas fa-bookmark fa-3x p-3 mr-4"></i>
                             <div class="media-body">
                                 <p>Create a user friendly environment</p>
-                            </div> 
-                        </div>                        
+                            </div>
+                        </div>
                     </section>
                 </main>
             </div>
