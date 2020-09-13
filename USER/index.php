2eaa3e5da5c4d52c3a64d3d08fdad160b915204c
Rutvik <bhimani.rutvik1211@gmail.com>
2020-09-13 12:11:09 +0530

    test

--- USER/index.php
+++ USER/index.php
@@ -10,17 +10,11 @@
     <link href="css/bootstrap.min.css" rel="stylesheet" /> <!-- https://getbootstrap.com/ -->
     <link href="fontawesome/css/all.min.css" rel="stylesheet" /> <!-- https://fontawesome.com/ -->
     <link href="css/templatemo-diagoona.css" rel="stylesheet" />
-<!--
 
-TemplateMo 550 Diagoona
-
-https://templatemo.com/tm-550-diagoona
-
--->
 </head>
 
 <body>
-    <div class="tm-container">        
+    <div class="tm-container">
         <div>
             <?php require_once 'header.php'; ?>
             <div class="tm-row">
@@ -30,12 +24,12 @@ https://templatemo.com/tm-550-diagoona
                         <h2 class="mb-5 tm-content-title">Diagoona HTML Template</h2>
                         <p class="mb-5">Diagoona is provided by TemplateMo website. You are allowed to use this template for your websites. You are NOT allowed to redistribute this template ZIP file for a download purpose on any template collection website.</p>
                         <hr class="mb-5">
-                        <p class="mb-5">Diagoona is Bootstrap v4.4.1 layout. This BG is 50% transparent. You can set the background images auto play settings (true or false in line number 33) in templatemo-script.js file in js folder.</p>                        
+                        <p class="mb-5">Diagoona is Bootstrap v4.4.1 layout. This BG is 50% transparent. You can set the background images auto play settings (true or false in line number 33) in templatemo-script.js file in js folder.</p>
                         <a href="about.php" class="btn btn-primary">Continue...</a>
                     </section>
                 </main>
             </div>
-        </div>        
+        </div>
 
         <?php require_once 'footer.php'; ?>
 
